1381bfa3a884a8941876ec0210bbaf5f5913ad70
Ben Pfaff <blp@cs.stanford.edu>
2017-01-26 14:43:08 -0800

    Implement search.

--- app.js
+++ app.js
@@ -2,18 +2,37 @@ var main = function() {
     $(".openbtn").click(function() {
         $("#sidenav").animate({left: "0"}, 250, function () { $(".sidenav-text").fadeIn() });
         $("#main").animate({paddingLeft: "250px"}, 250);
-        $("#head-wrap").animate({paddingLeft: "250px"}, 250);
+        $("#head").animate({paddingLeft: "250px"}, 250);
     });
     $(".closebtn").click(function() {
         $("#sidenav").animate({left: "-250px"}, 250,
                               function () { $(".sidenav-text").hide() });
         $("#main").animate({paddingLeft: "1%"}, 250);
-        $("#head-wrap").animate({paddingLeft: "1%"}, 250);
+        $("#head").animate({paddingLeft: "1%"}, 250);
     });
     $(".sidenav a").click(function() {
-        $("#sidenav").css("left", "-250px");
-        $("#main").css("paddingLeft", "1%");
-        $("#head-wrap").css({"paddingLeft": "1%"});
+        if ($(document).width() < 800) {
+            $("#sidenav").css("left", "-250px");
+            $("#main").css("paddingLeft", "1%");
+            $("#head").css({"paddingLeft": "1%"});
+        }
     });
+    function search(target) {
+        target = target.toLowerCase();
+        for (var i = 1; i <= 23; i++) {
+            var ptr = $("#pe" + i);
+            if ($("#e" + i).text().toLowerCase().indexOf(target) >= 0) {
+                ptr.show();
+            } else {
+                ptr.hide();
+            }
+        }
+        if (target == '') {
+            $("#episodes").text("Episodes");
+        } else {
+            $("#episodes").text("Search results:");
+        }
+    }
+    $("#search").on('input', function() { search($("#search").val()); });
 }
 $(document).ready(main);
