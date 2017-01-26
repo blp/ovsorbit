46f1f78033c04afc24f639fbc9f4d4c33dee39ba
Ben Pfaff <blp@cs.stanford.edu>
2017-01-26 13:55:26 -0800

    fixes

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ var main = function() {
         $("#main").animate({paddingLeft: "250px"}, 250);
         $("#head-wrap").animate({paddingLeft: "250px"}, 250);
     });
-    var closeSideNav = function() {
-        $("#sidenav").animate({left: "-250px"}, 250, function () { $(".sidenav-text").hide() });
+    $(".closebtn").click(function() {
+        $("#sidenav").animate({left: "-250px"}, 250,
+                              function () { $(".sidenav-text").hide() });
         $("#main").animate({paddingLeft: "1%"}, 250);
         $("#head-wrap").animate({paddingLeft: "1%"}, 250);
-    }
-    $(".closebtn").click(closeSideNav);
-    $(".sidenav a").click(function() { $("#sidenav").css("left", "-250px"); $("#main").css("paddingLeft", "1%"); });
+    });
+    $(".sidenav a").click(function() {
+        $("#sidenav").css("left", "-250px");
+        $("#main").css("paddingLeft", "1%");
+        $("#head-wrap").css({"paddingLeft": "1%"});
+    });
 }
 $(document).ready(main);
