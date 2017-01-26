a480ea23288cb2987ffdba4364edc28da857c353
Ben Pfaff <blp@cs.stanford.edu>
2017-01-26 13:48:20 -0800

    getting better

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var main = function() {
     $(".openbtn").click(function() {
         $("#sidenav").animate({left: "0"}, 250, function () { $(".sidenav-text").fadeIn() });
-        $("#main").animate({marginLeft: "250px"}, 250);
+        $("#main").animate({paddingLeft: "250px"}, 250);
+        $("#head-wrap").animate({paddingLeft: "250px"}, 250);
     });
-    $(".closebtn").click(function() {
+    var closeSideNav = function() {
         $("#sidenav").animate({left: "-250px"}, 250, function () { $(".sidenav-text").hide() });
-        $("#main").animate({marginLeft: "1%"}, 250);
-    });
+        $("#main").animate({paddingLeft: "1%"}, 250);
+        $("#head-wrap").animate({paddingLeft: "1%"}, 250);
+    }
+    $(".closebtn").click(closeSideNav);
+    $(".sidenav a").click(function() { $("#sidenav").css("left", "-250px"); $("#main").css("paddingLeft", "1%"); });
 }
 $(document).ready(main);
